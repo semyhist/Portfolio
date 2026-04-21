f125f187f04606e17501ee23360b70d420c61f3d
semyhist <Semih.aydin142@gmail.com>
2026-04-21 19:00:14 +0300

    Remove old React files and add Netlify config for Next.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-rootElement.render(<App />);
